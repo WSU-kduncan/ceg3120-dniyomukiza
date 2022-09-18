6ee3c6a6664498fc075f09a8c6b1e841991eea4d
Nididier <76929907+dniyomukiza@users.noreply.github.com>
2022-09-18 01:16:03 -0400

    couple changes sept18

--- code.py
+++ code.py
@@ -29,7 +29,7 @@ async def on_message(message):
     if message.author == client.user:
         return
 
-    brooklyn_99_quotes = [
+    question1_to_ask = [
        ' Hi! I\'m TeckTalk and I can answer to your questions relating the currenty technology and society.',
         'Hey,how can I help?',
         (
@@ -37,22 +37,46 @@ async def on_message(message):
         ),
     ]
 
-    hitchhiker_quotes = [
-        'There is an art, it says, or rather, a knack to flying. The knack lies in learning how to throw yourself at the ground and miss.',
-        'It is a mistake to think you can solve any major problems just with potatoes.',
-        'In the beginning the Universe was created. This has made a lot of people very angry and been widely regarded as a bad move.',
-        'A common mistake that people make when trying to design something completely foolproof is to underestimate the ingenuity of complete fools.',
+    good_answers_bot = [
+        'I do not really worry honestly.',
+        'Yes,technology providers do not provide as much digital security as they possibly can.',
+        'In fact I have never thought about this.',
+        'I do not worry about my privacy, if someone really wants your data they will get it.',
     ]
 
-    if message.content == 'Tell me your favorite technology tool and why you use it':
-    
-        await message.channel.send("Well, my favorite piece of technology is my phone because of the accessibility.  The ability to keep in contact with my friends and help track of my work and being able to look up things such as locations and information.")
 
-    if message.content == 'Hi!':
-        response = random.choice(brooklyn_99_quotes)
+    dictionary = [
+            'Was nice to have you!',
+            'Have a nice day!',
+            
+            ]
+    personal_questions = [
+              'I was born before the creation of the universe.',
+              '2000 years BE',
+              
+              ]
+    some_random_answer = [
+       'Well, my favorite piece of technology is my phone.',
+       'My favorite technology I use daily is google. Google is my friend.',
+       'The main favorite for me in technology would be my arcade stick.',
+        
+    ]
+    
+    if message.content == 'Tell me your favorite technology tool and why you use it':
+        response = random.choice(some_random_answer)
         await message.channel.send(response)
 
+    if message.content == 'Hi!':
+        response = random.choice(question1_to_ask)
+        await message.channel.send(response)
 
-
-
+    if message.content == 'Do you worry your technology provider is doing everything in their control to provide digital security?':
+        response = random.choice(good_answers_bot)
+        await message.channel.send(response)
+    if message.content == 'goodbye!':
+        response = random.choice(dictionary)
+        await message.channel.send(response)
+    if message.content == 'how old are you?':
+        response = random.choice(personal_questions)
+        await message.channel.send(response)
 client.run(TOKEN)
---
+++ discord-guide.md
@@ -0,0 +1,5 @@
+Do you worry your technology provider is doing everything in their control to provide digital security?
+Tell me your favorite technology tool and why you use it
+goobye!
+Hi!
+how old are you
